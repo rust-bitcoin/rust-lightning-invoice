271610680ffd0ac456b26aa93320419fd956921a
Sebastian Geisler <s.geisler@agdsn.de>
2018-03-08 23:59:59 +0100

    reformat code to use tabs (as does rust-lightning)

--- src/lib.rs
+++ src/lib.rs
@@ -19,112 +19,108 @@ mod parsers;
 
 /// An Invoice for a payment on the lightning network as defined in
 /// [BOLT #11](https://github.com/lightningnetwork/lightning-rfc/blob/master/11-payment-encoding.md).
-
 #[derive(Eq, PartialEq, Debug)]
 pub struct Invoice {
-    /// The currency deferred from the 3rd and 4th character of the bech32 transaction
-    pub currency: Currency,
+	/// The currency deferred from the 3rd and 4th character of the bech32 transaction
+	pub currency: Currency,
 
-    /// The amount to pay in pico-satoshis
-    pub amount: Option<u64>,
+	/// The amount to pay in pico-satoshis
+	pub amount: Option<u64>,
 
-    pub timestamp: DateTime<Utc>,
+	pub timestamp: DateTime<Utc>,
 
-    /// tagged fields of the payment request
-    pub tagged: Vec<TaggedField>,
+	/// tagged fields of the payment request
+	pub tagged: Vec<TaggedField>,
 
-    pub signature: Signature,
+	pub signature: Signature,
 }
 
 #[derive(Eq, PartialEq, Debug)]
 pub enum Currency {
-    Bitcoin,
-    BitcoinTestnet,
+	Bitcoin,
+	BitcoinTestnet,
 }
 
 #[derive(Eq, PartialEq, Debug)]
 pub enum TaggedField {
-    PaymentHash([u8; 32]),
-    Description(String),
-    PayeePubKey(PublicKey),
-    DescriptionHash([u8; 32]),
-    ExpiryTime(Duration),
-    MinFinalCltvExpiry(u64),
-    Fallback(Fallback),
-    Route {
-        pubkey: PublicKey,
-        short_channel_id: u64,
-        fee_base_msat: i32,
-        fee_proportional_millionths: i32,
-        cltv_expiry_delta: u16,
-    }
+	PaymentHash([u8; 32]),
+	Description(String),
+	PayeePubKey(PublicKey),
+	DescriptionHash([u8; 32]),
+	ExpiryTime(Duration),
+	MinFinalCltvExpiry(u64),
+	Fallback(Fallback),
+	Route {
+		pubkey: PublicKey,
+		short_channel_id: u64,
+		fee_base_msat: i32,
+		fee_proportional_millionths: i32,
+		cltv_expiry_delta: u16,
+	},
 }
 
 // TODO: better types instead onf byte arrays
 #[derive(Eq, PartialEq, Debug)]
 pub enum Fallback {
-    SegWitScript {
-        version: u8,
-        script: Vec<u8>,
-    },
-    PubKeyHash([u8; 20]),
-    ScriptHash([u8; 20]),
+	SegWitScript {
+		version: u8,
+		script: Vec<u8>,
+	},
+	PubKeyHash([u8; 20]),
+	ScriptHash([u8; 20]),
 }
 
 impl FromStr for Invoice {
-    type Err = parsers::Error;
+	type Err = parsers::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let Bech32 {hrp, data} = s.parse()?;
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let Bech32 { hrp, data } = s.parse()?;
 
-        let (currency, amount) = parsers::parse_hrp(&hrp)?;
+		let (currency, amount) = parsers::parse_hrp(&hrp)?;
 
-        Ok(Invoice {
-            currency,
-            amount,
-            timestamp: Utc::now(),
-            tagged: vec![],
-            signature: Signature::from_der(&Secp256k1::new(), &[0; 65])?,
-        })
-    }
+		Ok(Invoice {
+			currency,
+			amount,
+			timestamp: Utc::now(),
+			tagged: vec![],
+			signature: Signature::from_der(&Secp256k1::new(), &[0; 65])?,
+		})
+	}
 }
-
 
 
 impl Currency {
-    pub fn get_currency_prefix(&self) -> &'static str {
-        match self {
-            &Currency::Bitcoin => "bc",
-            &Currency::BitcoinTestnet => "tb",
-        }
-    }
+	pub fn get_currency_prefix(&self) -> &'static str {
+		match self {
+			&Currency::Bitcoin => "bc",
+			&Currency::BitcoinTestnet => "tb",
+		}
+	}
 
-    pub fn from_prefix(prefix: &str) -> Result<Currency, parsers::Error> {
-        match prefix {
-            "bc" => Ok(Currency::Bitcoin),
-            "tb" => Ok(Currency::BitcoinTestnet),
-            _ => Err(parsers::Error::UnknownCurrency)
-        }
-    }
+	pub fn from_prefix(prefix: &str) -> Result<Currency, parsers::Error> {
+		match prefix {
+			"bc" => Ok(Currency::Bitcoin),
+			"tb" => Ok(Currency::BitcoinTestnet),
+			_ => Err(parsers::Error::UnknownCurrency)
+		}
+	}
 }
 
 impl FromStr for Currency {
-    type Err = parsers::Error;
+	type Err = parsers::Error;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Currency::from_prefix(s)
-    }
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		Currency::from_prefix(s)
+	}
 }
-
-
 
 
 #[cfg(test)]
 mod test {
-    #[test]
-    fn test_currency_code() {
-        use super::Currency;
-        assert_eq!("bc", Currency::Bitcoin.get_currency_prefix());
-        assert_eq!("tb", Currency::BitcoinTestnet.get_currency_prefix());
-    }
+	#[test]
+	fn test_currency_code() {
+		use super::Currency;
+		assert_eq!("bc", Currency::Bitcoin.get_currency_prefix());
+		assert_eq!("tb", Currency::BitcoinTestnet.get_currency_prefix());
+	}
 }
--- src/parsers.rs
+++ src/parsers.rs
@@ -15,62 +15,62 @@ use secp256k1::{Signature, Secp256k1};
 use super::{Currency, TaggedField};
 
 pub(super) fn parse_hrp(hrp: &str) -> Result<(Currency, Option<u64>), Error> {
-    let re = Regex::new(r"^ln([^0-9]*)([0-9]*)([munp]?)$").unwrap();
-    let parts = match re.captures(&hrp) {
-        Some(capture_group) => capture_group,
-        None => return Err(Error::MalformedHRP)
-    };
+	let re = Regex::new(r"^ln([^0-9]*)([0-9]*)([munp]?)$").unwrap();
+	let parts = match re.captures(&hrp) {
+		Some(capture_group) => capture_group,
+		None => return Err(Error::MalformedHRP)
+	};
 
-    let currency = parts[0].parse::<Currency>()?;
+	let currency = parts[0].parse::<Currency>()?;
 
-    let amount = if !parts[1].is_empty() {
-        Some(parts[1].parse::<u64>()?)
-    } else {
-        None
-    };
+	let amount = if !parts[1].is_empty() {
+		Some(parts[1].parse::<u64>()?)
+	} else {
+		None
+	};
 
-    // `get_multiplier(x)` will only return `None` if `x` is not "m", "u", "n" or "p", which
-    // due to the above regex ensures that `get_multiplier(x)` iif `x == ""`, so it's ok to
-    // convert a none to 1BTC aka 10^12pBTC.
-    let multiplier = parts[2].chars().next().and_then(|suffix| {
-        get_multiplier(&suffix)
-    }).unwrap_or(1_000_000_000_000);
+	// `get_multiplier(x)` will only return `None` if `x` is not "m", "u", "n" or "p", which
+	// due to the above regex ensures that `get_multiplier(x)` iif `x == ""`, so it's ok to
+	// convert a none to 1BTC aka 10^12pBTC.
+	let multiplier = parts[2].chars().next().and_then(|suffix| {
+		get_multiplier(&suffix)
+	}).unwrap_or(1_000_000_000_000);
 
-    Ok((currency, amount.map(|amount| amount * multiplier)))
+	Ok((currency, amount.map(|amount| amount * multiplier)))
 }
 
 fn get_multiplier(multiplier: &char) -> Option<u64> {
-    match multiplier {
-        &'m' => Some(1_000_000_000),
-        &'u' => Some(1_000_000),
-        &'n' => Some(1_000),
-        &'p' => Some(1),
-        _ => None
-    }
+	match multiplier {
+		&'m' => Some(1_000_000_000),
+		&'u' => Some(1_000_000),
+		&'n' => Some(1_000),
+		&'p' => Some(1),
+		_ => None
+	}
 }
 
 // why &[u8] instead of Vec<u8>?: split_off reallocs => wouldn't save much cloning,
 // instead split_at is used, which doesn't need ownership of the data
 pub(super) fn parse_data(data: &[u8]) -> Result<(DateTime<Utc>, Vec<TaggedField>, Signature), Error> {
-    if data.len() < 104 + 7 { // signature + timestamp
-        return Err(Error::TooShortDataPart);
-    }
+	if data.len() < 104 + 7 { // signature + timestamp
+		return Err(Error::TooShortDataPart);
+	}
 
-    let (time, data) = data.split_at(8);
-    let (tagged, signature) = data.split_at(data.len() - 32);
-    assert_eq!(time.len(), 7);
-    assert_eq!(signature.len(), 104);
+	let (time, data) = data.split_at(8);
+	let (tagged, signature) = data.split_at(data.len() - 32);
+	assert_eq!(time.len(), 7);
+	assert_eq!(signature.len(), 104);
 
-    let time = Utc.timestamp(be_u64(time) as i64, 0);
-    let signature = Signature::from_compact(&Secp256k1::without_caps(), signature)?;
+	let time = Utc.timestamp(be_u64(time) as i64, 0);
+	let signature = Signature::from_compact(&Secp256k1::without_caps(), signature)?;
 	let tagged = parse_tagged_parts(tagged)?;
 
-    Ok((time, tagged, signature))
+	Ok((time, tagged, signature))
 }
 
 // interpret 5bit bech32 characters as big endian u64
 fn be_u64(bytes_5b: &[u8]) -> u64 {
-    bytes_5b.iter().fold(0, |acc, b| acc * 32 + (*b as u64))
+	bytes_5b.iter().fold(0, |acc, b| acc * 32 + (*b as u64))
 }
 
 fn parse_tagged_parts(data: &[u8]) -> Result<Vec<TaggedField>, Error> {
@@ -79,50 +79,50 @@ fn parse_tagged_parts(data: &[u8]) -> Result<Vec<TaggedField>, Error> {
 
 #[derive(PartialEq, Debug)]
 pub enum Error {
-    Bech32Error(bech32::Error),
-    ParseAmountError(ParseIntError),
-    MalformedSignature(secp256k1::Error),
-    BadPrefix,
-    UnknownCurrency,
-    MalformedHRP,
-    TooShortDataPart
+	Bech32Error(bech32::Error),
+	ParseAmountError(ParseIntError),
+	MalformedSignature(secp256k1::Error),
+	BadPrefix,
+	UnknownCurrency,
+	MalformedHRP,
+	TooShortDataPart,
 }
 
 impl Display for Error {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        use self::Error::*;
-        use std::error::Error;
-        match *self {
-            // TODO: find a way to combine the first three arms (e as error::Error?)
-            Bech32Error(ref e) => {
-                write!(f, "{} ({})", self.description(), e)
-            },
-            ParseAmountError(ref e) => {
-                write!(f, "{} ({})", self.description(), e)
-            },
-            MalformedSignature(ref e) => {
-                write!(f, "{} ({})", self.description(), e)
-            },
-            _ => {
-                write!(f, "{}", self.description())
-            }
-        }
-    }
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		use self::Error::*;
+		use std::error::Error;
+		match *self {
+			// TODO: find a way to combine the first three arms (e as error::Error?)
+			Bech32Error(ref e) => {
+				write!(f, "{} ({})", self.description(), e)
+			}
+			ParseAmountError(ref e) => {
+				write!(f, "{} ({})", self.description(), e)
+			}
+			MalformedSignature(ref e) => {
+				write!(f, "{} ({})", self.description(), e)
+			}
+			_ => {
+				write!(f, "{}", self.description())
+			}
+		}
+	}
 }
 
 impl error::Error for Error {
-    fn description(&self) -> &str {
-        use self::Error::*;
-        match *self {
-            Bech32Error(_) => "invalid bech32",
-            ParseAmountError(_) => "invalid amount in hrp",
-            MalformedSignature(_) => "invalid secp256k1 signature",
-            BadPrefix => "did not begin with 'ln'",
-            UnknownCurrency => "currency code unknown",
-            MalformedHRP => "malformed human readable part",
-            TooShortDataPart => "data part too short (should be at least 111 bech32 chars long)"
-        }
-    }
+	fn description(&self) -> &str {
+		use self::Error::*;
+		match *self {
+			Bech32Error(_) => "invalid bech32",
+			ParseAmountError(_) => "invalid amount in hrp",
+			MalformedSignature(_) => "invalid secp256k1 signature",
+			BadPrefix => "did not begin with 'ln'",
+			UnknownCurrency => "currency code unknown",
+			MalformedHRP => "malformed human readable part",
+			TooShortDataPart => "data part too short (should be at least 111 bech32 chars long)"
+		}
+	}
 }
 
 macro_rules! from_error {
