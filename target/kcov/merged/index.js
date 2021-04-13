fe070d68135742fff5547997ce295edb72d3c3de
Travis CI User <travis@example.org>
2021-04-13 12:35:09 +0000

    last kcov result

---
+++ target/kcov/merged/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+], merged_files:[{"link":"kcov-merged/index.html","title":"[merged]","summary_name":"[merged]","covered_class":"lineNoCov","covered":"0.0","covered_lines":"0","uncovered_lines":"0","total_lines" : "0"},
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2021-04-13 12:35:09", "instrumented" : 0, "covered" : 0,};
---
+++ target/kcov/merged/kcov-merged/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2021-04-13 12:35:09", "instrumented" : 0, "covered" : 0,};
+var merged_data = [];
